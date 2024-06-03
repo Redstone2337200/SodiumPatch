f3f290b1b1d5457e3de82742100b065ea72e1e42
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2024-06-03 22:26:45 +0800

    Add files via upload

--- src/main/java/me/jellysquid/mods/sodium/mixin/core/render/world/ChunkBuilderMixin.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/core/render/world/ChunkBuilderMixin.java
@@ -1,1 +1,22 @@
+package me.jellysquid.mods.sodium.mixin.core.render.world;
 
+import net.minecraft.client.render.chunk.ChunkBuilder;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+//import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ChunkBuilder.class)
+public class ChunkBuilderMixin {
+/*
+    @ModifyVariable(method = "<init>", index = 9, at = @At(value = "INVOKE", target = "Lcom/google/common/collect/Lists;newArrayListWithExpectedSize(I)Ljava/util/ArrayList;", remap = false))
+    private int modifyThreadPoolSize(int prev) {
+        // Do not allow any resources to be allocated
+        return 0;
+    }*/
+    @Inject(at = @At("HEAD"), method = "ChunkBuilder")
+	private void init(CallbackInfo info) {
+		// This code is injected into the start of MinecraftServer.loadWorld()V
+	}
+}
