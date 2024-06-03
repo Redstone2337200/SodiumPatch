1a9b8992149513b395e7dd129f58a225ae7e2a8d
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2024-06-03 22:38:16 +0800

    Update ClientWorldMixin.java

--- src/main/java/me/jellysquid/mods/sodium/mixin/core/world/biome/ClientWorldMixin.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/core/world/biome/ClientWorldMixin.java
@@ -12,13 +12,10 @@ import net.minecraft.util.profiler.Profiler;
 import net.minecraft.world.World;
 import net.minecraft.world.dimension.DimensionType;*/
 import org.spongepowered.asm.mixin.Mixin;
-/*import org.spongepowered.asm.mixin.Unique;
+//import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;*/
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
 
 //import java.util.function.Supplier;
 
